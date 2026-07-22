5184292c1d2abca9dc007b34dae6e811080c9970
nsdhnt1012 <2250545@ecc.ac.jp>
2026-07-22 12:17:27 +0900

    追加：ナビ選択時activeクラス付与

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,47 @@
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import navIconHome from '../assets/nav-icon-home.png';
 import navIconArticle from '../assets/nav-icon-article.png';
 import navIconData from '../assets/nav-icon-data.png';
 import navIconProduct from '../assets/nav-icon-product.png';
 
 function Header(){
+  // const url = location.pathname;
+  function navChange(navName){
+    console.log(navName);
+  }
+  
   return(
     <>
       <header>
         <nav>
-          <Link to="/" className='nav-link'>
+          <NavLink 
+            to="/" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
             <img src={navIconHome} alt="ホーム" />
             <p>ホーム</p>
-          </Link>
-          <Link to="/article" className='nav-link'>
+          </NavLink>
+          <NavLink 
+            to="/article" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
             <img src={navIconArticle} alt="記事一覧" />
             <p>記事一覧</p>
-          </Link>
-          <Link to="/data" className='nav-link'>
+          </NavLink>
+          <NavLink 
+            to="/data" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
             <img src={navIconData} alt="データ" />
             <p>データ</p>
-          </Link>
-          <Link to="/product" className='nav-link'>
+          </NavLink>
+          <NavLink 
+            to="/product" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
             <img src={navIconProduct} alt="制作リスト" />
             <p>制作リスト</p>
-          </Link>
+          </NavLink>
         </nav>
       </header>
     </>
