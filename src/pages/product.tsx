b31c5d2ef2431de382a04d724df28b20bf52bc20
nsdhnt1012 <2250545@ecc.ac.jp>
2026-07-23 19:47:57 +0900

    追加：制作リストスタイル

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -5,6 +5,35 @@ function Product() {
   return(
     <>
       <Header />
+      <main>
+        <div className="side-bar">
+          <nav className="product-nav">
+            <ul>
+              <li>Todoリスト</li>
+              <li>ポートフォリオ</li>
+              <li>模写コーディング</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="main-block">
+          <div className="article-list">
+            <article className='article-content'>
+              <a href="#">
+                <h1>01</h1>
+                <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                <p className='article-info'>
+                  <time>2026/5/26</time>
+                  <span>@honda-dev-jp</span>
+                  <span>128</span>
+                </p>
+                <p className='article-tag'>
+                  <span>CSS</span>
+                </p>
+              </a>
+            </article>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
