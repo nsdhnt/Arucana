aad204cfcc3e55e00220bd5dc75f9099d53a7eea
nsdhnt1012 <2250545@ecc.ac.jp>
2026-08-18 18:56:34 +0900

    修正：ヘッダー中身

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import './Header.css';
 import { NavLink } from 'react-router-dom';
 import navIconHome from '../assets/nav-icon-home.png';
-import navIconArticle from '../assets/nav-icon-article.png';
-import navIconData from '../assets/nav-icon-data.png';
-import navIconProduct from '../assets/nav-icon-product.png';
+import navIconSearch from '../assets/nav-icon-search.png';
+import navIconProject from '../assets/nav-icon-project.png';
+import navIconMemo from '../assets/nav-icon-memo.png';
+import navIconHistory from '../assets/nav-icon-history.png';
 
 function Header(){
   // const url = location.pathname;
@@ -22,21 +23,27 @@ function Header(){
             to="/article" 
             className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
           >
-            <img src={navIconArticle} alt="記事一覧" />
-            <p>記事一覧</p>
-          </NavLink>
-          <NavLink 
-            to="/data" 
-            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
-          >
-            <img src={navIconData} alt="データ" />
-            <p>データ</p>
+            <img src={navIconSearch} alt="検索" />
+            <p>検索</p>
           </NavLink>
           <NavLink 
             to="/product" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
+            <img src={navIconProject} alt="プロジェクト" />
+            <p>プロジェクト</p>
+          </NavLink>
+          <NavLink 
+            to="/memo" 
             className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
-            <img src={navIconProduct} alt="制作リスト" />
-            <p>制作リスト</p>
+            <img src={navIconMemo} alt="メモ" />
+            <p>メモ</p>
+          </NavLink>
+          <NavLink 
+            to="/history" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
+            <img src={navIconHistory} alt="履歴" />
+            <p>履歴</p>
           </NavLink>
         </nav>
       </header>
