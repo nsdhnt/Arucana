24f17520766ddd6ef02a567bfae7dc2003d50b0b
nsdhnt1012 <2250545@ecc.ac.jp>
2026-08-27 18:14:55 +0900

    追加：履歴画面スタイル途中

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -13,6 +13,88 @@ function History() {
   return(
     <>
       <Header setIsSidebarOpen={setIsSidebarOpen} setIsMobileSidebarOpen={setIsMobileSidebarOpen} />
+      <main>
+        <div className="book-shelf">
+          <div className="shelf-content-left">
+            <div className="ranking">
+              <h2>よく読んだ記事ランキング</h2>
+              <div className="shelf-wrap">
+                <article className='shelf-content'>
+                  <a href="#">
+                    <p>1</p>
+                    <div>
+                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-info'>
+                        <time>2026/5/26</time>
+                        <span>@honda-dev-jp</span>
+                        <span>128</span>
+                      </p>
+                      <p>開いた回数 72回</p>
+                    </div>
+                  </a>
+                </article>
+                <article className='shelf-content'>
+                  <a href="#">
+                    <p>2</p>
+                    <div>
+                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-info'>
+                        <time>2026/5/26</time>
+                        <span>@honda-dev-jp</span>
+                        <span>128</span>
+                      </p>
+                      <p>開いた回数 72回</p>
+                    </div>
+                  </a>
+                </article>
+                <article className='shelf-content'>
+                  <a href="#">
+                    <p>3</p>
+                    <div>
+                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-info'>
+                        <time>2026/5/26</time>
+                        <span>@honda-dev-jp</span>
+                        <span>128</span>
+                      </p>
+                      <p>開いた回数 72回</p>
+                    </div>
+                  </a>
+                </article>
+                <article className='shelf-content'>
+                  <a href="#">
+                    <p>4</p>
+                    <div>
+                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-info'>
+                        <time>2026/5/26</time>
+                        <span>@honda-dev-jp</span>
+                        <span>128</span>
+                      </p>
+                      <p>開いた回数 72回</p>
+                    </div>
+                  </a>
+                </article>
+                <article className='shelf-content'>
+                  <a href="#">
+                    <p>5</p>
+                    <div>
+                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-info'>
+                        <time>2026/5/26</time>
+                        <span>@honda-dev-jp</span>
+                        <span>128</span>
+                      </p>
+                      <p>開いた回数 72回</p>
+                    </div>
+                  </a>
+                </article>
+              </div>
+            </div>
+          </div>
+          <div className="shelf-content-right"></div>
+        </div>
+      </main>
     </>
   )
 }
