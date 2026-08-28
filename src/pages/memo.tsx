166a8b91307ba436d0c4c8e94537da3dd2bcd340
nsdhnt1012 <2250545@ecc.ac.jp>
2026-08-28 15:08:44 +0900

    追加：メモ画面見た目

--- src/pages/memo.tsx
+++ src/pages/memo.tsx
@@ -13,6 +13,24 @@ function Memo() {
   return(
     <>
       <Header setIsSidebarOpen={setIsSidebarOpen} setIsMobileSidebarOpen={setIsMobileSidebarOpen} />
+      <main>
+        <ul className="memo-shelf-list">
+          <li className="memo-shelf">
+            <div className="memo-content">
+              <h3>【Tailwind CSS入門 Day16】詳細モーダルを作る</h3>
+              <p className="memo-txt">複雑そうに見えて、思ったより簡単だった</p>
+              <p className="memo-date">2026/09/04</p>
+            </div>
+          </li>
+          <li className="memo-shelf">
+            <div className="memo-content">
+              <h3>【Tailwind CSS入門 Day16】詳細モーダルを作る</h3>
+              <p className="memo-txt">複雑そうに見えて、思ったより簡単だった</p>
+              <p className="memo-date">2026/09/04</p>
+            </div>
+          </li>
+        </ul>
+      </main>
     </>
   )
 }
