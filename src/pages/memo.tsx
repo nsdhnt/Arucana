da2bf6009950b91c58b4d1912b27e6dda5ff61ef
nsdhnt1012 <2250545@ecc.ac.jp>
2026-09-01 17:48:16 +0900

    追加：メモ表示

--- src/pages/memo.tsx
+++ src/pages/memo.tsx
@@ -1,21 +1,46 @@
 import "../pages/memo.css";
 import Header from "../components/Header";
-import { useState } from 'react';
+import React, { memo, useState } from 'react';
+
+interface Memos {
+  // id: number;
+  title: string;
+  text: string;
+  date: string;
+}
+
+interface MemoProps {
+  memos: Memos[];
+  setMemos: React.Dispatch<React.SetStateAction<Memos[]>>;
+}
 
 
-function Memo() {
+function Memo({ memos, setMemos }: MemoProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState<boolean>(false);
 
-  console.log(isSidebarOpen);
-  console.log(isMobileSidebarOpen);
+  // console.log(isSidebarOpen);
+  // console.log(isMobileSidebarOpen);
+
+  console.log(memos);
+  console.log(setMemos);
+  
 
   return(
     <>
       <Header setIsSidebarOpen={setIsSidebarOpen} setIsMobileSidebarOpen={setIsMobileSidebarOpen} />
       <main>
         <ul className="memo-shelf-list">
-          <li className="memo-shelf">
+          {[...memos].reverse().map((memo) => (
+            <li className="memo-shelf">
+              <div className="memo-content">
+                <h3>{memo.title}</h3>
+                <p className="memo-txt">{memo.text}</p>
+                <p className="memo-date">{memo.date}</p>
+              </div>
+            </li>
+          ))}
+          {/* <li className="memo-shelf">
             <div className="memo-content">
               <h3>【Tailwind CSS入門 Day16】詳細モーダルを作る</h3>
               <p className="memo-txt">複雑そうに見えて、思ったより簡単だった</p>
@@ -25,10 +50,10 @@ function Memo() {
           <li className="memo-shelf">
             <div className="memo-content">
               <h3>【Tailwind CSS入門 Day16】詳細モーダルを作る</h3>
-              <p className="memo-txt">複雑そうに見えて、思ったより簡単だった</p>
+              <p className="memo-txt">複雑そうに見えて、思ったより簡単だった複雑そうに見えて、思ったより簡単だった複雑そうに見えて、思ったより簡単だった複雑そうに見えて、思ったより簡単だった複雑そうに見えて、思ったより簡単だった複雑そうに見えて、思ったより簡単だった</p>
               <p className="memo-date">2026/09/04</p>
             </div>
-          </li>
+          </li> */}
         </ul>
       </main>
     </>
