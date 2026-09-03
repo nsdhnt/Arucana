38d98616cebf168b8fe24e72d1d2f345a15a7d45
nsdhnt1012 <2250545@ecc.ac.jp>
2026-09-03 18:48:20 +0900

    追加：履歴画面の木目画像、スタイル変更

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -37,13 +37,13 @@ function History() {
                   <a href="#">
                     <p className="ranking-id">2</p>
                     <div>
-                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-title'>手軽に使えるCSSアニメーション</p>
                       <p className='article-info'>
                         <time>2026/5/26</time>
                         <span>@honda-dev-jp</span>
                         <span>128いいね</span>
                       </p>
-                      <p className="count">開いた回数 <span>72</span> 回</p>
+                      <p className="count">開いた回数 <span>64</span> 回</p>
                     </div>
                   </a>
                 </article>
@@ -51,13 +51,13 @@ function History() {
                   <a href="#">
                     <p className="ranking-id">3</p>
                     <div>
-                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-title'>useEffectをとりあえず理解する</p>
                       <p className='article-info'>
                         <time>2026/5/26</time>
                         <span>@honda-dev-jp</span>
                         <span>128いいね</span>
                       </p>
-                      <p className="count">開いた回数 <span>72</span> 回</p>
+                      <p className="count">開いた回数 <span>45</span> 回</p>
                     </div>
                   </a>
                 </article>
@@ -65,13 +65,13 @@ function History() {
                   <a href="#">
                     <p className="ranking-id">4</p>
                     <div>
-                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-title'>useStateで配列要素を追加・削除・変更する方法</p>
                       <p className='article-info'>
                         <time>2026/5/26</time>
                         <span>@honda-dev-jp</span>
                         <span>128いいね</span>
                       </p>
-                      <p className="count">開いた回数 <span>72</span> 回</p>
+                      <p className="count">開いた回数 <span>31</span> 回</p>
                     </div>
                   </a>
                 </article>
@@ -79,13 +79,13 @@ function History() {
                   <a href="#">
                     <p className="ranking-id">5</p>
                     <div>
-                      <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                      <p className='article-title'>PHPでフォームからデータ送る</p>
                       <p className='article-info'>
                         <time>2026/5/26</time>
                         <span>@honda-dev-jp</span>
                         <span>128いいね</span>
                       </p>
-                      <p className="count">開いた回数 <span>72</span> 回</p>
+                      <p className="count">開いた回数 <span>16</span> 回</p>
                     </div>
                   </a>
                 </article>
@@ -94,31 +94,33 @@ function History() {
           </div>
           <div className="shelf-content-right">
             <h2>閲覧履歴</h2>
-            <article className='article-content'>
-              <a href="#">
-                <h1>01</h1>
-                <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
-                <p className='article-info'>
-                  <time>2026/5/26</time>
-                  <span>@honda-dev-jp</span>
-                  <span>128いいね</span>
-                </p>
-                <p className='article-tag'>
-                  <span>CSS</span>
-                </p>
-              </a>
-            </article>
-            <article className='article-content'>
-              <a href="#">
-                <h1>02</h1>
-                <p className='article-title'>CSS @keyframes をビジュアル編集するツールを作った — タイムライン UI と「アニメーションを再起動する」CSS のワナ</p>
-                <p className='article-info'>
-                  <time>2026/5/26</time>
-                  <span>@sen-ltd</span>
-                  <span>128いいね</span>
-                </p>
-              </a>
-            </article>
+            <div className="article-list">
+              <article className='article-content'>
+                <a href="#">
+                  <h1>01</h1>
+                  <p className='article-title'>【Tailwind CSS入門 Day16】詳細モーダルを作る</p>
+                  <p className='article-info'>
+                    <time>2026/5/26</time>
+                    <span>@honda-dev-jp</span>
+                    <span>128いいね</span>
+                  </p>
+                  <p className='article-tag'>
+                    <span>CSS</span>
+                  </p>
+                </a>
+              </article>
+              <article className='article-content'>
+                <a href="#">
+                  <h1>02</h1>
+                  <p className='article-title'>CSS @keyframes をビジュアル編集するツールを作った — タイムライン UI と「アニメーションを再起動する」CSS のワナ</p>
+                  <p className='article-info'>
+                    <time>2026/5/26</time>
+                    <span>@sen-ltd</span>
+                    <span>128いいね</span>
+                  </p>
+                </a>
+              </article>
+            </div>
           </div>
         </div>
       </main>
